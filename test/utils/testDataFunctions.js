3e17e9c97306567642bd66bb37d0f739ebd4e4ba
Chris Johnson <chrisjohnson@equalexperts.com>
2025-05-01 09:57:29 +0100

    Fix length of generateRandomMRN

--- test/utils/testDataFunctions.js
+++ test/utils/testDataFunctions.js
@@ -1,8 +1,9 @@
 export function generateRandomMRN(prefix = '25GB') {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
   let result = prefix
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < 14; i++) {
     result += chars.charAt(Math.floor(Math.random() * chars.length))
   }
+  console.log(result)
   return result
 }
