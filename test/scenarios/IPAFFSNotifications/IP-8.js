045b920897fa5ceb47d9967c4f09c50e68d8be0d
Francis Chelladurai <francis.chelladurai@equalexperts.com>
2025-09-19 17:23:06 +0100

    IP-n tests are automated (#28)
    * IP-n tests are automated

--- test/scenarios/IPAFFSNotifications/IP-8.js
+++ test/scenarios/IPAFFSNotifications/IP-8.js
@@ -1,5 +1,48 @@
 describe('BTMS receives an IPAFFS CHED-PP that requires both a PHSI & HMI (SMS) inspection - IP-8', function () {
-  it.skip('', async function () {
+  it('', async function () {
     this.timeout(70000)
+
+    // Create a ched-pp with multiple commodities
+    this.docRef = generateDocumentReference({
+      letter: 'PP',
+      prefixLength: 4,
+      suffixLength: 7
+    })
+    this.mrn = generateRandomMRN()
+
+    await sendIpaffsMessage(
+      loadIPAFFSJson('CHEDPP_SMS.json', {
+        version: 1,
+        referenceNumber: this.docRef,
+        lastUpdated: new Date().toISOString(),
+        partTwo: {
+          decision: {},
+          inspectionRequired: 'Required'
+        }
+      })
+    )
+    testLogger.info('✓ A new IPAFFS notification sent successfully')
+
+    // Send a Clearance request
+    await newFluentClearanceRequestTest()
+      .addItem({
+        TaricCommodityCode: '0702001007',
+        GoodsDescription: 'Cherry tomatoes',
+        Documents: [
+          { DocumentCode: 'N851', DocumentReference: this.docRef },
+          { DocumentCode: 'N002', DocumentReference: this.docRef }
+        ],
+        Checks: [
+          { CheckCode: 'H219', DepartmentCode: 'PHSI' },
+          { CheckCode: 'H218', DepartmentCode: 'HMI' }
+        ]
+      })
+      .withMRN(this.mrn)
+      .withEntryVersionNumber(1)
+      .sendClearanceRequest()
+      .then(async (test) => {
+        await test.waitForCheckDecision('H218', 'H02')
+        await test.waitForCheckDecision('H219', 'H02')
+      })
   })
 })
