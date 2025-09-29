4cd85746dc2904dfb4f371898cf848d86a0241c5
Francis Chelladurai <francis.chelladurai@equalexperts.com>
2025-09-29 13:22:31 +0100

    Updated new tests to use fluent
    Signed-off-by: Francis Chelladurai <francis.chelladurai@equalexperts.com>

--- test/scenarios/GMS/gms.js
+++ test/scenarios/GMS/gms.js
@@ -158,33 +158,42 @@ describe('GMS', function () {
         )
       )
 
-      const builder = new SoapMessageBuilder()
-      builder.addItem({
-        TaricCommodityCode: '0103911000',
-        Documents: allDocuments,
-        Checks: checks
-      })
-
       this.mrn = generateRandomMRN()
-      const soapEnvelope = builder.buildMessage({ mrn: this.mrn })
 
-      const firstExpectedDecision = expected?.[0]?.decisionCode ?? decisionCode
+      await newClearanceRequest()
+        .addItem({
+          TaricCommodityCode: '0103911000',
+          Documents: allDocuments,
+          Checks: checks
+        })
+        .withMRN(this.mrn)
+        .sendClearanceRequest()
+        .then(async (test) => {
+          const firstExpectedDecision =
+            expected?.[0]?.decisionCode ?? decisionCode
 
-      await sendSoapRequest(SUBMIT_CLEARANCE_REQUEST_ENDPOINT, soapEnvelope)
-      const decisionXml = await waitForSpecificDecision(
-        this.mrn,
-        firstExpectedDecision
-      )
-      const codes = await extractDecisionCodes(decisionXml)
+          if (expected?.length > 1) {
+            // Wait for multiple decisions
+            for (const expectedPair of expected) {
+              await test.waitForCheckDecision(
+                expectedPair.checkCode,
+                expectedPair.decisionCode
+              )
+            }
+          } else {
+            // Wait for single decision
+            if (expected?.[0]?.checkCode) {
+              await test.waitForCheckDecision(
+                expected[0].checkCode,
+                firstExpectedDecision
+              )
+            } else {
+              await test.waitForDecision(firstExpectedDecision)
+            }
+          }
 
-      testLogger.info('Received decision codes:', { decisionCodes: codes })
-
-      const expectedPairs = expected?.length
-        ? expected
-        : [{ checkCode: checks[0].CheckCode, decisionCode }]
-      expectedPairs.forEach((pair) => {
-        expect(codes).to.deep.include(pair)
-      })
+          testLogger.info('Received all expected decisions')
+        })
     })
   })
 })
